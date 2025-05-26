46bbccfadc3e364e13f60fff3af21670d7807660
Denis <Fedoseev.ds@mail.ru>
2025-05-26 19:35:49 +0700

    Changes in accordance with PEP8 recommendations. Изменения в соответствии с рекомендациями PEP8.

--- module1part2level3.py
+++ module1part2level3.py
@@ -1,8 +1,13 @@
-password = str(input('Введите пароль. Длина пароля должна быть не менее 8 символов. Пароль должен содержать прописные и заглавные буквы: '))
-length = len(password)
+password = str(input('Введите пароль. Длина пароля должна быть не менее 8 '
+                     'символов. Пароль должен содержать прописные '
+                     'и заглавные буквы: '))
+password_length = len(password)
 
-while length < 8 or password.islower() == True or password.isupper() == True or password.isalpha() != True:
-    password = str(input('Длина пароля должна быть не менее 8 символов. Пароль должен содержать прописные и заглавные буквы: '))
-    length = len(password)
+while (password_length < 8 or password.islower() == True or
+       password.isupper() == True or password.isalpha() != True):
+    password = str(input('Введите пароль. Длина пароля должна быть не '
+                         'менее 8 символов. Пароль должен содержать '
+                         'прописные и заглавные буквы: '))
+    password_length = len(password)
 else:
-    print('Введен допустимый пароль')
+    print('Введен допустимый пароль.')
