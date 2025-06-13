6e389fd84d86fc22bd72f2ea45c2d6dbf2951ce7
DenisDSF <fedoseev.ds@mail.ru>
2025-06-13 14:27:34 +0700

    Delete module3 directory

--- module3/module3part2level1.py
+++
@@ -1,6 +0,0 @@
-data_list = [1, 4, 1, 6, 'hello', 'a', 5, 'hello', 1, 'hello']
-intersections_list =[]
-for i in data_list:
-    if data_list.count(i) > 1 and i not in intersections_list:
-        intersections_list.append(i)
-print(intersections_list)
