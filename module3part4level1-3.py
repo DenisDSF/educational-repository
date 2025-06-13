91aff4a88e60f8e843172b101865bd0a9b40a2a9
Denis <Fedoseev.ds@mail.ru>
2025-06-13 16:12:03 +0700

    Functions fixed.

--- module3part4level1-3.py
+++ module3part4level1-3.py
@@ -3,7 +3,7 @@ import os
 
 
 def checking_file_existence(file):
-    if os.path.exists(file) != True:
+    if not os.path.exists(file):
         with open(file, 'w') as f:
             empty_dict = {}
             json.dump(empty_dict, f)
@@ -13,7 +13,7 @@ def register(login, password):
     with open('user_data.json', 'r') as f:
         user_data = json.load(f)
     if login in user_data.keys():
-        print('Пользователь с таким логином уже существует.')
+        return False
     else:
         user_data[login] = password
         with open('user_data.json', 'w') as f:
@@ -24,10 +24,9 @@ def logging_in(login, password):
     with open('user_data.json', 'r') as f:
         user_data = json.load(f)
         if login in user_data.keys() and user_data[login] == password:
-            print('Вы вошли!')
-            return 'выход'
+            return True
         else:
-            print('Неверный логин или пароль.')
+            return False
 
 
 checking_file_existence('user_data.json')
@@ -36,12 +35,16 @@ while True:
     if user_choice == 'регистрация':
         login = input('Введите логин: ')
         password = input('Введите пароль: ')
-        register(login, password)
+        if register(login, password) == False:
+            print('Пользователь с таким логином уже существует.')
     elif user_choice == 'вход':
         login = input('Введите логин: ')
         password = input('Введите пароль: ')
-        if logging_in(login, password) == 'выход':
+        if logging_in(login, password) == True:
+            print('Вы вошли!')
             break
+        else:
+            print('Неверный логин или пароль.')
     elif user_choice == 'выход':
         break
     else:
