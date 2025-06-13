a623107d7296fee575af5ee0aa16522b32c26871
Denis <Fedoseev.ds@mail.ru>
2025-06-13 15:22:06 +0700

    Function 'checking_digits' has been removed. Функция 'checking_digits' удалена.

--- module3part1level3.py
+++ module3part1level3.py
@@ -1,13 +1,8 @@
-def checking_digits(x):
-    while x.isdigit() != True:
-        x = input('Введеные данные не являются целым числом. '
-                  'Введите целое число: ')
-    return x
-
-
 num = input('Введите целое число: ')
 if num.isdigit() != True:
-    num = checking_digits(num)
+    while num.isdigit() != True:
+        num = input('Введеные данные не являются целым числом. '
+                  'Введите целое число: ')
 nums_summ = 0
 for i in num:
     nums_summ += int(i)
