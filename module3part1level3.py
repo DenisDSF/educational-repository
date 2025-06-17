62af31925ba56e469890481ce60edb8842101343
Denis <Fedoseev.ds@mail.ru>
2025-06-17 17:16:14 +0700

    Function "checking_digits" restored and redesigned. Функция "checking_digits" восстановлена и переработана.

--- module3part1level3.py
+++ module3part1level3.py
@@ -1,8 +1,14 @@
+def checking_digits(x):
+    if x.isdigit():
+        return True
+    else:
+        return False
+
+
 num = input('Введите целое число: ')
-if num.isdigit() != True:
-    while num.isdigit() != True:
-        num = input('Введеные данные не являются целым числом. '
-                  'Введите целое число: ')
+while checking_digits(num) != True:
+    num = input('Введеные данные не являются целым числом. '
+              'Введите целое число: ')
 nums_summ = 0
 for i in num:
     nums_summ += int(i)
